de7e200ccbcdd1b6d098781004e6787ba53cca9a
IremOnart <78277981+IremOnart@users.noreply.github.com>
2021-06-28 23:56:54 +0300

    Add files via upload

---
+++ panel/panel.php
@@ -0,0 +1,352 @@
+<?php 
+    include "includes/header.php";
+?>
+
+    <img class="resim" src="blog.jpg" alt="resim">
+    <br><br><br><br><br><br>
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6 col-sm-12 col-md-3">
+                <div class="kutu">
+                    <img src="1.jpg" alt="">
+                    <h2 class="yazı-boyutu" text-decoration: none;>
+                        <a href="#" class="basyazı">Open Source Job Report Show More Openings Fewer</a>
+                    </h2>
+                    <p class="yazı-boyutu">
+                        January 10,2020 admin
+                    </p>
+                    <p class="yazı-boyutu">
+                        We denounce with righteous indige nation and dislike men who are so beguiled and demo realized
+                        by
+                        the charms of pleasure of the moment, so blinded by desire, that...
+                    </p>
+
+                    <p class="yazı-boyutu">
+                        <a href="#" class="cc">Continue Reading</a>
+
+                    </p>
+                </div>
+
+
+                <br><br><br>
+                <div class="kutu">
+                    <img src="2.jpg" alt="">
+                    <h2 class="yazı-boyutu" text-decoration: none;>
+                        <a href="#" class="basyazı">Tech Products That Makes Its Easier to Stay at Home</a>
+                    </h2>
+                    <p class="yazı-boyutu">
+                        January 10,2020 admin
+                    </p>
+                    <p class="yazı-boyutu">
+                        We denounce with righteous indige nation and dislike men who are so beguiled and demo realized
+                        by
+                        the charms of pleasure of the moment, so blinded by desire, that...
+                    </p>
+
+                    <p class="yazı-boyutu">
+                        <a href="#" class="cc">Continue Reading</a>
+
+                    </p>
+                </div>
+
+
+                <br><br><br>
+                <div class="kutu">
+                    <img src="3.jpg" alt="">
+                    <h2 class="yazı-boyutu" text-decoration: none;>
+                        <a href="#" class="basyazı">Necessity May Give Us Your Best Virtual Court System</a>
+                    </h2>
+                    <p class="yazı-boyutu">
+                        January 10,2020 admin
+                    </p>
+                    <p class="yazı-boyutu">
+                        We denounce with righteous indige nation and dislike men who are so beguiled and demo realized
+                        by
+                        the charms of pleasure of the moment, so blinded by desire, that...
+                    </p>
+
+                    <p class="yazı-boyutu">
+                        <a href="#" class="cc">Continue Reading</a>
+
+                    </p>
+                </div>
+
+
+                <br><br><br>
+                <div class="kutu">
+                    <img src="4.jpg" alt="">
+                    <h2 class="yazı-boyutu" text-decoration: none;>
+                        <a href="#" class="basyazı">Servo Project Joins The Linux Foundation Fold Desco</a>
+                    </h2>
+                    <p class="yazı-boyutu">
+                        January 10,2020 admin
+                    </p>
+                    <p class="yazı-boyutu">
+                        We denounce with righteous indige nation and dislike men who are so beguiled and demo realized
+                        by
+                        the charms of pleasure of the moment, so blinded by desire, that...
+                    </p>
+
+                    <p class="yazı-boyutu">
+                        <a href="#" class="cc">Continue Reading</a>
+
+                    </p>
+                </div>
+
+
+                <br><br><br>
+                <div class="kutu">
+                    <img src="5.jpg" alt="">
+                    <h2 class="yazı-boyutu" text-decoration: none;>
+                        <a href="#" class="basyazı">Servo Project Joins The Linux Foundation Fold Desco</a>
+                    </h2>
+                    <p class="yazı-boyutu">
+                        January 10,2020 admin
+                    </p>
+                    <p class="yazı-boyutu">
+                        We denounce with righteous indige nation and dislike men who are so beguiled and demo realized
+                        by
+                        the charms of pleasure of the moment, so blinded by desire, that...
+                    </p>
+
+                    <p class="yazı-boyutu">
+                        <a href="#" class="cc">Continue Reading</a>
+
+                    </p>
+                </div>
+
+
+                <br><br><br>
+                <div class="kutu">
+                    <img src="6.jpg" alt="">
+                    <h2 class="yazı-boyutu" text-decoration: none;>
+                        <a href="#" class="basyazı">Necessity May Give Us Your Best Virtual Court System</a>
+                    </h2>
+                    <p class="yazı-boyutu">
+                        January 10,2020 admin
+                    </p>
+                    <p class="yazı-boyutu">
+                        We denounce with righteous indige nation and dislike men who are so beguiled and demo realized
+                        by
+                        the charms of pleasure of the moment, so blinded by desire, that...
+                    </p>
+
+                    <p class="yazı-boyutu">
+                        <a href="#" class="cc">Continue Reading</a>
+
+                    </p>
+                </div>
+
+
+                <br><br><br>
+                <div class="kutu">
+                    <img src="7.jpg" alt="">
+                    <h2 class="yazı-boyutu" text-decoration: none;>
+                        <a href="#" class="basyazı">Tech Products That Makes Its Easier to Stay at Home</a>
+                    </h2>
+                    <p class="yazı-boyutu">
+                        January 10,2020 admin
+                    </p>
+                    <p class="yazı-boyutu">
+                        We denounce with righteous indige nation and dislike men who are so beguiled and demo realized
+                        by
+                        the charms of pleasure of the moment, so blinded by desire, that...
+                    </p>
+
+                    <p class="yazı-boyutu">
+                        <a href="#" class="cc">Continue Reading</a>
+
+                    </p>
+                </div>
+
+
+                <br><br><br>
+                <div class="kutu">
+                    <img src="8.jpg" alt="">
+                    <h2 class="yazı-boyutu" text-decoration: none;>
+                        <a href="#" class="basyazı">Open Source Job Report Show More Openings Fewer</a>
+                    </h2>
+                    <p class="yazı-boyutu">
+                        January 10,2020 admin
+                    </p>
+                    <p class="yazı-boyutu">
+                        We denounce with righteous indige nation and dislike men who are so beguiled and demo realized
+                        by
+                        the charms of pleasure of the moment, so blinded by desire, that...
+                    </p>
+
+                    <p class="yazı-boyutu">
+                        <a href="#" class="cc">Continue Reading</a>
+
+                    </p>
+                </div>
+
+
+                <br><br><br>
+                <div class="kutu">
+                    <img src="9.jpg" alt="">
+                    <h2 class="yazı-boyutu" text-decoration: none;>
+                        <a href="#" class="basyazı">Types of Social Proof What its Makes Them Effective</a>
+                    </h2>
+                    <p class="yazı-boyutu">
+                        January 10,2020 admin
+                    </p>
+                    <p class="yazı-boyutu">
+                        We denounce with righteous indige nation and dislike men who are so beguiled and demo realized
+                        by
+                        the charms of pleasure of the moment, so blinded by desire, that...
+                    </p>
+
+                    <p class="yazı-boyutu">
+                        <a href="#" class="cc">Continue Reading</a>
+
+                    </p>
+                </div>
+
+
+                <br><br><br>
+                <div class="kutu">
+                    <img src="10.jpg" alt="">
+                    <h2 class="yazı-boyutu">
+                        <a href="#" class="basyazı">Tech Firms Support Huawei Restriction, Balk at Cost</a>
+                    </h2>
+                    <p class="yazı-boyutu">
+                        January 10,2020 admin
+                    </p>
+                    <p class="yazı-boyutu">
+                        We denounce with righteous indige nation and dislike men who are so beguiled and demo realized
+                        by
+                        the charms of pleasure of the moment, so blinded by desire, that...
+                    </p>
+
+                    <p class="yazı-boyutu">
+                        <a href="#" class="cc">Continue Reading</a>
+
+                    </p>
+                </div>
+            </div>
+
+
+
+
+            <div class="col-lg-6 col-sm-12 col-md-3">
+                
+                    <div class="ilk">
+                        <form class="example" action="action_page.php">
+                            <input type="text" placeholder="Search.." name="search">
+                            <button type="submit"><i class="fa fa-search"></i></button>
+                          </form>
+
+                    </div>
+                    <div class="ikinci">
+                        <br><br>
+                        <h2>
+                            Lastest Post
+                        </h2>
+                        <br>
+                        
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <a href="#"><img src="1.jpg" alt="" width="100"></a>
+                                
+
+
+                            </div>
+                            <div class="col-lg-6">
+                                <p class="ilkyazi">
+                                    <a href="#" class="dd" > Pen Source Job Report Show More Openings Fewer</a>
+                                    
+                                </p>
+                                
+                            </div>
+
+                        </div><br>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <a href="#"><img src="2.jpg" alt="" width="100"></a>
+                                
+
+
+                            </div>
+                            <div class="col-lg-6">
+                                <a href="#" class="dd" >Tech Products That Makes Its Easier to Stay at Home</a>
+                                
+                                
+                            </div>
+
+                        </div><br><br>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <a href="#"><img src="3.jpg" alt="" width="100"></a>
+                                
+
+
+                            </div>
+                            <div class="col-lg-6">
+
+                                <a href="#" class="dd" >Necessity May Give Us Your Best Virtual Court System</a>
+                                        
+                            </div>
+                                
+                        </div> <br><br>
+                        
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <a href="#"><img src="4.jpg" alt="" width="100"></a>
+                                
+
+
+                            </div>
+                            <div class="col-lg-6">
+
+                                <a href="#" class="dd" >Servo Project Joins The Linux Foundation Fold Desco</a>
+                                
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="ucuncu">
+                        <br>
+                        <h2>
+                            Categories
+                        </h2><br>
+                        <p>
+                            <a href="#" class="dd" >Artifical Intelligence</a>
+                            
+                        </p><br>
+                        <p>
+                            <a href="#" class="dd" >Digital Technology</a>
+                            
+                        </p><br>
+                        <p>
+                            <a href="#" class="dd" >IT Services</a>
+                            
+                        </p><br>
+                        <p>
+                            <a href="#" class="dd" >Software Development</a>
+                            
+                        </p><br>
+                        <p>
+                            <a href="#" class="dd" > Web Development</a>
+                           
+                        </p>
+                        
+                    
+                </div>
+
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <br><br><br><br>
+
+<?php 
+    include "includes/footer.php";
+?>
